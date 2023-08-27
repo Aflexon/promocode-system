678e3316bb056df30aea3ae19b412edcdc38d46c
Alex Afanasenko <i@aflexon.com>
2023-08-27 22:26:40 +0200

    Update workspace.sh, change docker compose command to version 2

--- workspace.sh
+++ workspace.sh
@@ -3,7 +3,7 @@ args=""
 
 tty -s && args="-it"
 
-docker-compose run ${args} \
+docker compose run ${args} \
  -v "${HOME}/.composer:/.composer" \
  --user=$(id -u) \
  --entrypoint /usr/src/docker-resources/entrypoint.sh \
