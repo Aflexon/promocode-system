ae2f0a1e9ed06280c80fd7ac06143e09cdd63a25
Alex Afanasenko <i@aflexon.com>
2023-08-27 22:46:54 +0200

    Add index for idx_received_at field

--- docker-resources/db/init-db.sh
+++ docker-resources/db/init-db.sh
@@ -17,7 +17,8 @@ do
     \`ip\` varchar(45) DEFAULT NULL,
     PRIMARY KEY (\`code\`),
     UNIQUE KEY \`idx_user\` (\`user_id\`),
-    KEY \`idx_ip\` (\`ip\`)
+    KEY \`idx_ip\` (\`ip\`),
+    KEY \`idx_received_at\` (\`received_at\`),
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;"
 done
 
